5ccb368543ce64a06fc761d6f9e0ec4db12dfae5
Dawid Gierowski <gi3r4s@gmail.com>
2021-02-01 10:29:52 +0100

    WIP, current version ...

--- regressor.py
+++ regressor.py
@@ -3,6 +3,8 @@ import sys
 import copy
 import random
 import itertools
+import time
+import math
 
 
 def get_actual_value(input, weights):
@@ -11,7 +13,8 @@ def get_actual_value(input, weights):
         actual_value += weights[i] * input[i]
     return actual_value
 
-new_max =  1
+
+new_max = 1
 new_min = -1
 
 
@@ -23,19 +26,20 @@ def denormalize_from_1_1(min_a, max_a, y_new):
     return (((y_new - new_min) / (new_max - new_min)) * (max_a - min_a)) + min_a
 
 
-def normalize_mk(min_a, max_a, y_new):
+def normalize_0_1(min_a, max_a, y_new):
     return (2 * (y_new - min_a) / (max_a - min_a)) - 1
 
 
-def denormalize_mk(min_a, max_a, y_old):
+def denormalize_0_1(min_a, max_a, y_old):
     return ((y_old + 1) / 2) * (max_a - min_a) + min_a
 
 
 normalized = normalize_to_1_1(-10, 10, 9)
 denormalized = denormalize_from_1_1(-10, 10, normalized)
 
-normalized_mk = normalize_mk(-10, 10, 9)
-denormalized_mk = denormalize_mk(-10, 10, normalized_mk)
+normalized_mk = normalize_0_1(-10, 10, 9)
+denormalized_mk = denormalize_0_1(-10, 10, normalized_mk)
+
 
 def get_component_value(exponents, inputs):
     value = 1
@@ -110,12 +114,18 @@ if __name__ == '__main__':
     # normalize all training and validation sets start
     for j in range(len(BASE_TRAIN_SET)):
         for k in range(NUMBER_OF_COLUMNS):
-            BASE_TRAIN_SET[j][k] = normalize_to_1_1(TRAINING_SET_MIN_MAXS[k][0], TRAINING_SET_MIN_MAXS[k][1],
-                                               BASE_TRAIN_SET[j][k])
+            BASE_TRAIN_SET[j][k] = normalize_0_1(TRAINING_SET_MIN_MAXS[k][0], TRAINING_SET_MIN_MAXS[k][1],
+                                                    BASE_TRAIN_SET[j][k])
 
+    # PARAMS START
     TRAINING_VALIDATION_PAIRS = []
-    NUMBER_OF_CHECKS = 5
+    NUMBER_OF_CHECKS = 16
+    MAX_NUMBER_OF_ITERATIONS = 675
+    EPSILON = 0.001
+    LEARNING_RATE = 0.02
+    MSE_SCORES = {}
 
+    # shuffle sets
     for i in range(NUMBER_OF_CHECKS):
         base_train_set_copy = copy.deepcopy(BASE_TRAIN_SET)
         random.shuffle(base_train_set_copy)
@@ -131,16 +141,17 @@ if __name__ == '__main__':
 
         TRAINING_VALIDATION_PAIRS.append((current_training_set, current_validation_set))
 
-    # PARAMS START
-    # MAX_NUMBER_OF_ITERATIONS = 10
-    MAX_NUMBER_OF_ITERATIONS = 10000
-    EPSILON = 0.001
-    LEARNING_RATE = 0.01
-    MSE_SCORES = {}
-
+    # start = time.time()
     # best k selection start
     combinations = []
+    is_error_met = False
     for k in range(1, 9):
+
+        if is_error_met:
+            for i in range(k-1, 9):
+                MSE_SCORES[i] = 9999999
+                # print("Total validation MSE: " + MSE_SCORES[i].__str__())
+            break
         # print("Processing k == " + k.__str__())
         arr1 = [i for i in range(0, k + 1)]
 
@@ -150,10 +161,14 @@ if __name__ == '__main__':
         for exponents in exponents_all:
             if sum(exponents) <= k:
                 EXPONENTS.append(exponents)
-        avg_mse = 0
+        total_mse = 0
+        pair_index = 0
         for training_part, validation_part in TRAINING_VALIDATION_PAIRS:
             current_iteration = 0
+            best_mse = 10000000000
+            iterations_without_best_mse = 0
             weights = [random.uniform(0, 1) for i in range(len(EXPONENTS))]
+            weights_len = len(weights)
             while True:
                 validation_mse = 0
                 for train_set_entry in training_part:
@@ -162,7 +177,7 @@ if __name__ == '__main__':
                         adjusted_values.append(get_component_value(exponents, train_set_entry[0:NUMBER_OF_COLUMNS - 1]))
                     prediction = get_actual_value(weights, adjusted_values)
                     diff = prediction - train_set_entry[-1]
-                    for i in range(len(weights)):
+                    for i in range(weights_len):
                         derivative = diff * adjusted_values[i]
                         weights[i] -= LEARNING_RATE * derivative
                 for validation_entry in validation_part:
@@ -174,23 +189,44 @@ if __name__ == '__main__':
                     diff = prediction - validation_entry[-1]
                     validation_mse += (diff * diff)
                 validation_mse /= len(validation_part)
-                if current_iteration >= MAX_NUMBER_OF_ITERATIONS or validation_mse <= EPSILON:
+
+                if math.isnan(validation_mse) or math.isinf(validation_mse):
+                    break
+
+                if validation_mse < best_mse:
+                    iterations_without_best_mse = 0
+                    best_mse = validation_mse
+                else:
+                    iterations_without_best_mse += 1
+
+                if current_iteration >= MAX_NUMBER_OF_ITERATIONS or validation_mse <= EPSILON or iterations_without_best_mse > 0.333 * MAX_NUMBER_OF_ITERATIONS:
                     break
                 current_iteration += 1
             # print("Validation MSE: " + validation_mse.__str__())
-            avg_mse += validation_mse
-            if not len(MSE_SCORES) == 0 and (avg_mse / NUMBER_OF_CHECKS) > min(MSE_SCORES.values()):
-                avg_mse = NUMBER_OF_CHECKS * validation_mse
+            pair_index += 1
+            total_mse += validation_mse
+
+            if math.isnan(validation_mse) or math.isnan(total_mse) or math.isinf(validation_mse) or math.isinf(total_mse):
+                is_error_met = True
                 break
-        avg_mse /= len(TRAINING_VALIDATION_PAIRS)
-        # print("AVG validation MSE: " + avg_mse.__str__())
-        MSE_SCORES[k] = avg_mse
+            if not len(MSE_SCORES) == 0 and total_mse > min(MSE_SCORES.values()):
+                # print("Before: " + total_mse.__str__())
+                total_mse += ((total_mse / pair_index) * (NUMBER_OF_CHECKS - pair_index))
+                # print("After: " + total_mse.__str__())
+                break
+
+        if not is_error_met:
+            # print("Total validation MSE: " + total_mse.__str__())
+            MSE_SCORES[k] = total_mse
+
+    # end = time.time()
+    # print("Time")
+    # print(end - start)
 
     # final model training parameters
-    RESULT_MAX_ITERATIONS = 50000
-    # RESULT_MAX_ITERATIONS = 50000
-    RESULT_LEARNING_RATE = 0.01
-    RESULT_EPSILON = 0.0001
+    RESULT_MAX_ITERATIONS = 30000
+    RESULT_LEARNING_RATE = 0.05
+    RESULT_EPSILON = 1e-8
     BEST_K = min(MSE_SCORES.items(), key=lambda t: t[1])
 
     # generate combinations of variables
@@ -225,7 +261,7 @@ if __name__ == '__main__':
             ITERATIONS_WITHOUT_BETTER_MSE = 0
         else:
             ITERATIONS_WITHOUT_BETTER_MSE += 1
-        if RESULT_CURRENT_ITERATION >= RESULT_MAX_ITERATIONS or result_mse < RESULT_EPSILON or ITERATIONS_WITHOUT_BETTER_MSE == 1000:
+        if RESULT_CURRENT_ITERATION >= RESULT_MAX_ITERATIONS or result_mse < RESULT_EPSILON or (ITERATIONS_WITHOUT_BETTER_MSE >= RESULT_MAX_ITERATIONS * 0.1):
             break
         RESULT_CURRENT_ITERATION += 1
 
@@ -255,7 +291,7 @@ if __name__ == '__main__':
 
     for i in range(len(INPUTS)):
         for k in range(len(INPUTS[i])):
-            INPUTS[i][k] = normalize_to_1_1(INPUTS_MIN_MAXS[k][0], INPUTS_MIN_MAXS[k][1], INPUTS[i][k])
+            INPUTS[i][k] = normalize_0_1(INPUTS_MIN_MAXS[k][0], INPUTS_MIN_MAXS[k][1], INPUTS[i][k])
 
     predictions = []
     for input in INPUTS:
@@ -266,7 +302,6 @@ if __name__ == '__main__':
         predictions.append(prediction)
 
     for i in range(len(predictions)):
-        denormalized_value = denormalize_from_1_1(TRAINING_SET_MIN_MAXS[-1][0], TRAINING_SET_MIN_MAXS[-1][1], predictions[i])
-        # diff = denormalized_value - EXPECTED_OUTPUTS[i]
-        # print("Diff: " + diff.__str__())
+        denormalized_value = denormalize_0_1(TRAINING_SET_MIN_MAXS[-1][0], TRAINING_SET_MIN_MAXS[-1][1],
+                                                  predictions[i])
         print(denormalized_value)
